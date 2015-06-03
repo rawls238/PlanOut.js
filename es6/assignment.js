e8589a9868c015cf7487d8a4e685ff35380dc95f
Guy <garidor@bu.edu>
2015-06-03 19:04:18 -0400

    normal spacing

--- es6/assignment.js
+++ es6/assignment.js
@@ -2,33 +2,33 @@ import { PlanOutOpRandom } from "./ops/random";
 import { shallowCopy, forEach } from "./lib/utils";
 
 class Assignment {
-	constructor(experiment_salt, overrides) {
+  constructor(experiment_salt, overrides) {
     if (!overrides) {
       overrides = {};
     }
-		this.experiment_salt = experiment_salt;
-		this._overrides = shallowCopy(overrides);
-		this._data = shallowCopy(overrides);
-	}
+    this.experiment_salt = experiment_salt;
+    this._overrides = shallowCopy(overrides);
+    this._data = shallowCopy(overrides);
+  }
 
-	evaluate(value) {
-		return value;
-	}
+  evaluate(value) {
+    return value;
+  }
 
-	get_overrides() {
-		return this._overrides;
-	}
+  get_overrides() {
+    return this._overrides;
+  }
 
-	set_overrides(overrides) {
-		this._overrides = shallowCopy(overrides);
-		var self = this;
-		forEach(Object.keys(this._overrides), function(override_key) {
-			self._data[override_key] = self._overrides[override_key];
-		});
-	}
+  set_overrides(overrides) {
+    this._overrides = shallowCopy(overrides);
+    var self = this;
+    forEach(Object.keys(this._overrides), function(override_key) {
+      self._data[override_key] = self._overrides[override_key];
+    });
+  }
 
-	set(name, value) {
-		if (name === '_data') {
+  set(name, value) {
+    if (name === '_data') {
       this._data = value;
       return;
     } else if (name === '_overrides') {
@@ -39,29 +39,29 @@ class Assignment {
       return;
     }
  
-		if (this._overrides[name]) {
+    if (this._overrides[name]) {
       return;
     }
     if (value instanceof PlanOutOpRandom) {
-    	if (!value.args.salt) {
-    		value.args.salt = name;
-    	}
-    	this._data[name] = value.execute(this);
+      if (!value.args.salt) {
+        value.args.salt = name;
+      }
+      this._data[name] = value.execute(this);
     } else {
       this._data[name] = value;
     }
   }
 
   get(name) {
-   	if (name === '_data') {
+    if (name === '_data') {
       return this._data;
     } else if( name === '_overrides') {
       return this._overrides;
     } else if ( name === 'experiment_salt') {
       return this.experiment_salt;
-   	} else {
-   		return this._data[name];
-   	}
+    } else {
+      return this._data[name];
+    }
   }
 
   get_params() {
@@ -69,15 +69,15 @@ class Assignment {
   }
 
   del(name) {
-  	delete this._data[name];
+    delete this._data[name];
   }
 
   to_string() {
-  	return String(this._data);
+    return String(this._data);
   }
 
   length() {
-  	return Object.keys(this._data).length;
+    return Object.keys(this._data).length;
   }
 };
 
