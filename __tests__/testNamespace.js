9c35bbb3f794ae2851404117b1122d0ba3a72589
Matt Rheault <matt@mattrheault.com>
2016-07-21 13:40:21 -0400

    Make all unit tests work w/ core compatible bundle

--- __tests__/testNamespace.js
+++ __tests__/testNamespace.js
@@ -1,9 +1,34 @@
-var Namespace = require('../es6/namespace.js');
-var Experiment = require('../es6/experiment.js');
 var Utils = require('../es6/lib/utils.js');
-var ExperimentSetup = require('../es6/experimentSetup');
+var Namespace = require.requireActual('../dist/planout.js').Namespace;
+var Experiment = require.requireActual('../dist/planout.js').Experiment;
+var ExperimentSetup = require.requireActual('../dist/planout.js').ExperimentSetup;
+var NamespaceCompat = require.requireActual('../dist/planout_core_compatible.js').Namespace;
+var ExperimentCompat = require.requireActual('../dist/planout_core_compatible.js').Experiment;
+var ExperimentSetupCompat = require.requireActual('../dist/planout_core_compatible.js').ExperimentSetup;
+
 
 class BaseExperiment extends Experiment {
+  configureLogger() {
+    return;
+  }
+
+  log(stuff) {
+    globalLog.push(stuff);
+  }
+
+  previouslyLogged() {
+    return;
+  }
+
+  getParamNames() {
+    return this.getDefaultParamNames();
+  }
+  setup() {
+    this.name = 'test_name';
+  }
+};
+
+class BaseExperimentCompat extends ExperimentCompat {
   configureLogger() {
     return;
   }
@@ -44,6 +69,25 @@ class Experiment3 extends BaseExperiment {
   }
 }
 
+class Experiment1Compat extends BaseExperimentCompat {
+  assign(params, args) {
+    params.set('test', 1)
+  }
+}
+
+class Experiment2Compat extends BaseExperimentCompat {
+
+  assign(params, args) {
+    params.set('test', 2)
+  }
+}
+
+class Experiment3Compat extends BaseExperimentCompat {
+  assign(params, args) {
+    params.set("test2", 3)
+  }
+}
+
 class BaseTestNamespace extends Namespace.SimpleNamespace {
   setup() {
     this.setName('test');
@@ -54,6 +98,18 @@ class BaseTestNamespace extends Namespace.SimpleNamespace {
     this.numSegments = 100;
   }
 };
+
+class BaseTestNamespaceCompat extends NamespaceCompat.SimpleNamespace {
+  setup() {
+    this.setName('test');
+    this.setPrimaryUnit('userid');
+  }
+
+  setupDefaults() {
+    this.numSegments = 100;
+  }
+};
+
 
 describe("Test namespace module", function() {
   var validateLog;
@@ -81,6 +137,7 @@ describe("Test namespace module", function() {
   afterEach(function() {
     globalLog = [];
   });
+
   it('Adds segment correctly', function() {
     class TestNamespace extends BaseTestNamespace {
       setupExperiments() {
@@ -95,8 +152,41 @@ describe("Test namespace module", function() {
     validateSegments(namespace, { Experiment1: 100 });
   });
 
+  it('Adds segment correctly (compat)', function() {
+    class TestNamespace extends BaseTestNamespaceCompat {
+      setupExperiments() {
+        this.addExperiment('Experiment1', Experiment1, 100);
+      }
+    };
+    var namespace = new TestNamespace({'userid': 'blah'});
+    expect(namespace.get('test')).toEqual(1);
+    expect(namespace.availableSegments.length).toEqual(0);
+    expect(Object.keys(namespace.segmentAllocations).length).toEqual(100);
+    validateLog("Experiment1");
+    validateSegments(namespace, { Experiment1: 100 });
+  });
+
   it('Adds two segments correctly', function() {
     class TestNamespace extends BaseTestNamespace {
+      setupExperiments() {
+        this.addExperiment('Experiment1', Experiment1, 50);
+        this.addExperiment('Experiment2', Experiment2, 50);
+      }
+    };
+
+    var namespace = new TestNamespace({'userid': 'blah'});
+    expect(namespace.get('test')).toEqual(1);
+    validateLog("Experiment1");
+    globalLog = [];
+    var namespace2 = new TestNamespace({'userid': 'abb'});
+    expect(namespace2.get('test')).toEqual(2);
+    validateLog("Experiment2");
+    var segValidation = { Experiment1: 50, Experiment2: 50};
+    validateSegments(namespace, segValidation);
+  });
+
+  it('Adds two segments correctly (compat)', function() {
+    class TestNamespace extends BaseTestNamespaceCompat {
       setupExperiments() {
         this.addExperiment('Experiment1', Experiment1, 50);
         this.addExperiment('Experiment2', Experiment2, 50);
@@ -137,9 +227,52 @@ describe("Test namespace module", function() {
     var namespace = new TestNamespace({'userid': str});
     validateSegments(namespace, { Experiment2: 10 });
   });
+
+  it('Can remove segment correctly (compat)', function() {
+    class TestNamespace extends BaseTestNamespaceCompat {
+      setupDefaults() {
+        this.numSegments = 10;
+      }
+
+      setupExperiments() {
+        this.addExperiment('Experiment1', Experiment1, 10);
+        this.removeExperiment('Experiment1');
+        this.addExperiment('Experiment2', Experiment2, 10);
+      }
+    };
+
+    var str = "bla";
+    for(var i = 0; i < 100; i++) {
+      str += "h";
+      var namespace = new TestNamespace({'userid': str});
+      expect(namespace.get('test')).toEqual(2);
+      validateLog("Experiment2");
+    }
+    var namespace = new TestNamespace({'userid': str});
+    validateSegments(namespace, { Experiment2: 10 });
+  });
   
   it('Should only log exposure when user could be in experiment', function() {
     class TestNamespace extends BaseTestNamespace {
+      setupDefaults() {
+        this.numSegments = 10;
+      }
+
+      setupExperiments() {
+        this.addExperiment('Experiment1', Experiment1, 5);
+        this.addExperiment('Experiment3', Experiment3, 5);
+      }
+    }
+
+    var namespace = new TestNamespace({'userid': 'hi'});
+    expect(namespace.get('test2')).toEqual(3);
+    expect(globalLog.length).toEqual(1);
+    expect(namespace.get('test'));
+    validateLog("Experiment3");
+  });
+
+  it('Should only log exposure when user could be in experiment (compat)', function() {
+    class TestNamespace extends BaseTestNamespaceCompat {
       setupDefaults() {
         this.numSegments = 10;
       }
@@ -190,6 +323,39 @@ describe("Test namespace module", function() {
     validateLog('Experiment3');
   });
   
+  it('Allow experiment overrides in SimpleNamespace (compat)', function() {
+    class TestNamespace extends BaseTestNamespaceCompat {
+      setupExperiments() {
+        this.addExperiment('Experiment1', Experiment1, 50);
+        this.addExperiment('Experiment3', Experiment3, 50);
+      }
+
+      allowedOverride() { 
+        return true;
+      }
+
+      getOverrides() {
+        return {
+          'test': {
+            'experimentName': 'Experiment1',
+            'value': 'overridden'
+          },
+          'test2': {
+            'experimentName': 'Experiment3',
+            'value': 'overridden2'
+          }
+        };
+      }
+    }
+
+    var namespace = new TestNamespace({'userid': 'hi'});
+    expect(namespace.get('test')).toEqual('overridden');
+    validateLog('Experiment1');
+    globalLog = [];
+    expect(namespace.get('test2')).toEqual('overridden2');
+    validateLog('Experiment3');
+  });
+
   it('should respect auto exposure logging being set to off', function() { 
     class ExperimentNoExposure extends BaseExperiment {
       setup() {
@@ -202,6 +368,28 @@ describe("Test namespace module", function() {
       }
     };
     class TestNamespace extends BaseTestNamespace {
+      setupExperiments() {
+        this.addExperiment('ExperimentNoExposure', ExperimentNoExposure, 100);
+      }
+    };
+
+    var namespace = new TestNamespace({'userid': 'hi'});
+    namespace.get('test');
+    expect(globalLog.length).toEqual(0);
+  });
+
+  it('should respect auto exposure logging being set to off (compat)', function() { 
+    class ExperimentNoExposure extends BaseExperimentCompat {
+      setup() {
+        this.setAutoExposureLogging(false);
+        this.name = 'test_name';
+      }
+
+      assign(params, args) {
+        params.set('test', 1)
+      }
+    };
+    class TestNamespace extends BaseTestNamespaceCompat {
       setupExperiments() {
         this.addExperiment('ExperimentNoExposure', ExperimentNoExposure, 100);
       }
@@ -240,6 +428,34 @@ describe("Test namespace module", function() {
     expect(globalLog.length).toEqual(1);
   });
 
+  it('should respect dynamic getParamNames (compat)', function() {
+    class ExperimentParamTest extends Experiment1Compat {
+
+      assign(params, args) {
+        let clonedArgs = Utils.shallowCopy(args);
+        delete clonedArgs.userid;
+        let keys = Object.keys(clonedArgs);
+        Utils.forEach(keys, function(key) {
+          params.set(key, 1);
+        });
+      }
+
+      getParamNames() {
+        return ['foo', 'bar'];
+      }
+    };
+    class TestNamespace extends BaseTestNamespaceCompat {
+      setupExperiments() {
+        this.addExperiment('ExperimentParamTest', ExperimentParamTest, 100);
+      }
+    };
+    var namespace = new TestNamespace({'userid': 'hi', 'foo': 1, 'bar': 1});
+    namespace.get('test');
+    expect(globalLog.length).toEqual(0);
+    namespace.get('foo');
+    expect(globalLog.length).toEqual(1);
+  });
+
   it('should work with getParams', () => {
     class SimpleExperiment extends BaseExperiment {
       assign(params, args) {
@@ -252,6 +468,31 @@ describe("Test namespace module", function() {
       }
     };
     class TestNamespace extends BaseTestNamespace {
+      setupExperiments() {
+        return;
+      }
+    }
+    var namespace = new TestNamespace({'userid': 'hi', 'foo': 1, 'bar': 1});
+    namespace.getParams('SimpleExperiment');
+    expect(globalLog.length).toEqual(0);
+    var namespace2 = new TestNamespace2({'userid': 'hi', 'foo': 1, 'bar': 1});
+    var params = namespace2.getParams('SimpleExperiment');
+    expect(globalLog.length).toEqual(1);
+    expect(params).toEqual({'test': 1});
+  });
+
+  it('should work with getParams (compat)', () => {
+    class SimpleExperiment extends BaseExperimentCompat {
+      assign(params, args) {
+        params.set('test', 1)
+      }
+    };
+    class TestNamespace2 extends BaseTestNamespaceCompat {
+      setupExperiments() {
+        this.addExperiment('SimpleExperiment', SimpleExperiment, 100);
+      }
+    };
+    class TestNamespace extends BaseTestNamespaceCompat {
       setupExperiments() {
         return;
       }
@@ -283,6 +524,24 @@ describe("Test namespace module", function() {
     expect(globalLog.length).toEqual(1);
   });
 
+  it('should only log exposure if "get" is called on a valid param (compat)', function() {
+    class SimpleExperiment extends BaseExperimentCompat {
+      assign(params, args) {
+        params.set('test', 1)
+      }
+    };
+    class TestNamespace extends BaseTestNamespaceCompat {
+      setupExperiments() {
+        this.addExperiment('SimpleExperiment', SimpleExperiment, 100);
+      }
+    };
+    var namespace = new TestNamespace({'userid': 'hi', 'foo': 1, 'bar': 1});
+    namespace.get('foobar');
+    expect(globalLog.length).toEqual(0);
+    expect(namespace.get('test')).toBe(1);
+    expect(globalLog.length).toEqual(1);
+  });
+
   it('should work with experiment setup', function() {
     class SimpleExperiment extends BaseExperiment {
       assign(params, args) {
@@ -296,6 +555,23 @@ describe("Test namespace module", function() {
     };
     var namespace = new TestNamespace({'foo': 1, 'bar': 1});
     ExperimentSetup.registerExperimentInput('userid', 'hi');
+    expect(namespace.get('test')).toBe(1);
+    expect(globalLog.length).toEqual(1);
+  });
+
+  it('should work with experiment setup (compat)', function() {
+    class SimpleExperiment extends BaseExperimentCompat {
+      assign(params, args) {
+        params.set('test', 1)
+      }
+    };
+    class TestNamespace extends BaseTestNamespaceCompat {
+      setupExperiments() {
+        this.addExperiment('SimpleExperiment', SimpleExperiment, 100);
+      }
+    };
+    var namespace = new TestNamespace({'foo': 1, 'bar': 1});
+    ExperimentSetupCompat.registerExperimentInput('userid', 'hi');
     expect(namespace.get('test')).toBe(1);
     expect(globalLog.length).toEqual(1);
   });
@@ -315,10 +591,39 @@ describe("Test namespace module", function() {
         this.addExperiment('Experiment1', Experiment1, 6);
       }
     }
+
     var count = 0;
     var total = 10000;
     for (var i = 0; i < total; i++) {
       ExperimentSetup.registerExperimentInput('userid', i);
+      var n = new TestNamespaces();
+      if (n.get('test')) {
+        count += 1;
+      }
+    }
+    expect(count >= 5500 && count <= 6500).toBe(true);
+  });
+
+  it('actually works (compat)', function() {
+    class TestNamespaces extends BaseTestNamespaceCompat {
+      setup() {
+        this.setName('testomg');
+        this.setPrimaryUnit('userid');
+      }
+
+      setupDefaults() {
+        this.numSegments = 10;
+      }
+
+      setupExperiments() {
+        this.addExperiment('Experiment1', Experiment1, 6);
+      }
+    }
+
+    var count = 0;
+    var total = 10000;
+    for (var i = 0; i < total; i++) {
+      ExperimentSetupCompat.registerExperimentInput('userid', i);
       var n = new TestNamespaces();
       if (n.get('test')) {
         count += 1;
--- __tests__/testRandomOps.js
+++ __tests__/testRandomOps.js
@@ -1,5 +1,7 @@
-var Assignment = require('../es6/assignment');
-var Random = require('../es6/ops/random');
+var Assignment = require.requireActual('../dist/planout.js').Assignment;
+var Random = require.requireActual('../dist/planout.js').Ops.Random;
+var AssignmentCompat = require.requireActual('../dist/planout_core_compatible.js').Assignment;
+var RandomCompat = require.requireActual('../dist/planout_core_compatible.js').Ops.Random;
 
 var z = 3.29;
 
@@ -78,7 +80,30 @@ describe('Test randomization ops', function() {
     a.set('f', new Random.RandomInteger({ 'min': 0, 'max': 100000, 'unit': i, 'full_salt':'fs2'}));
     b.set('f', new Random.RandomInteger({ 'min': 0, 'max': 100000, 'unit': i, 'full_salt':'fs2'}));
     expect(a.get('f')).toEqual(b.get('f'));
+  });
 
+  it('salts correctly (compat)', function() {
+    var i = 20;
+    var a = new AssignmentCompat("assign_salt_a");
+
+    a.set('x', new RandomCompat.RandomInteger({ 'min': 0, 'max': 100000, 'unit': i}));
+    a.set('y', new RandomCompat.RandomInteger({ 'min': 0, 'max': 100000, 'unit': i}));
+    expect(a.get('x')).not.toEqual(a.get('y'));
+
+    a.set('z', new RandomCompat.RandomInteger({ 'min': 0, 'max': 100000, 'unit': i, 'salt': 'x'}));
+    expect(a.get('x')).toEqual(a.get('z'));
+
+    var b = new AssignmentCompat('assign_salt_b');
+    b.set('x', new RandomCompat.RandomInteger({ 'min': 0, 'max': 100000, 'unit': i}));
+    expect(a.get('x')).not.toEqual(b.get('x'));
+
+    a.set('f', new RandomCompat.RandomInteger({ 'min': 0, 'max': 100000, 'unit': i, 'full_salt':'fs'}));
+    b.set('f', new RandomCompat.RandomInteger({ 'min': 0, 'max': 100000, 'unit': i, 'full_salt':'fs'}));
+    expect(a.get('f')).toEqual(b.get('f'));
+
+    a.set('f', new RandomCompat.RandomInteger({ 'min': 0, 'max': 100000, 'unit': i, 'full_salt':'fs2'}));
+    b.set('f', new RandomCompat.RandomInteger({ 'min': 0, 'max': 100000, 'unit': i, 'full_salt':'fs2'}));
+    expect(a.get('f')).toEqual(b.get('f'));
   });
 
   it('works for bernoulli trials', function() {
@@ -88,6 +113,23 @@ describe('Test randomization ops', function() {
       for (var i = 0; i < N; i++) {
         var a = new Assignment(p);
         a.set('x', new Random.BernoulliTrial({ 'p': p, 'unit': i }));
+        xs[i] = a.get('x');
+      }
+      return xs;
+    }
+
+    distributionTester(bernoulli(0.0), [{0: 1}, {1: 0}], N);
+    distributionTester(bernoulli(0.1), [{0: 0.9}, {1: 0.1}], N);
+    distributionTester(bernoulli(1.0), [{0: 0}, {1: 1}], N);
+  });
+
+  it('works for bernoulli trials (compat)', function() {
+    var N = 10000;
+    function bernoulli(p) {
+      var xs = [];
+      for (var i = 0; i < N; i++) {
+        var a = new AssignmentCompat(p);
+        a.set('x', new RandomCompat.BernoulliTrial({ 'p': p, 'unit': i }));
         xs[i] = a.get('x');
       }
       return xs;
@@ -118,6 +160,26 @@ describe('Test randomization ops', function() {
     testDistributions();
   });
 
+  it('works for uniform choice (compat)', function() {
+    var N = 10000;
+    function uniformChoice(choices) {
+      var xs = [];
+      for (var i = 0; i < N; i++) {
+        var a = new AssignmentCompat(choices.join(','));
+        a.set('x', new RandomCompat.UniformChoice({ 'choices': choices, 'unit': i }));
+        xs[i] = a.get('x');
+      }
+      return xs;
+    }
+
+    function testDistributions() {
+      distributionTester(uniformChoice(['a']), [{'a': 1}], N);
+      distributionTester(uniformChoice(['a', 'b']), [{'a': 1}, {'b': 1}], N);
+    };
+
+    testDistributions();
+  });
+
   it('works for weighted choice', function() {
     var N = 10000;
     function weightedChoice(choices) {
@@ -127,6 +189,36 @@ describe('Test randomization ops', function() {
       for (var i = 0; i < N; i++) {
         var a = new Assignment(weights.join(', '));
         a.set('x', new Random.WeightedChoice({ 'choices': choices, 'weights': weights, 'unit': i }));
+        xs[i] = a.get('x');
+      }
+      return xs;
+    }
+
+    function testDistributions() {
+      var d = [{'a': 1}];
+      distributionTester(weightedChoice(d), d, N);
+      d = [{'a': 1}, {'b': 2}];
+      distributionTester(weightedChoice(d), d, N);
+      d = [{'a': 0}, {'b': 2}, {'c': 0}];
+      distributionTester(weightedChoice(d), d, N);
+
+      var da = [{'a': 1}, {'b': 2}, {'c': 0}, {'a': 2}];
+      var db = [{'a': 3}, {'b': 2}, {'c': 0}];
+      distributionTester(weightedChoice(da), db, N);
+    }
+
+    testDistributions();
+  });
+
+  it('works for weighted choice (compat)', function() {
+    var N = 10000;
+    function weightedChoice(choices) {
+      var xs = [];
+      var weights = choices.map(function(choice) { return choice[Object.keys(choice)[0]]});
+      var choices = choices.map(function(choice) { return Object.keys(choice)[0]; });
+      for (var i = 0; i < N; i++) {
+        var a = new AssignmentCompat(weights.join(', '));
+        a.set('x', new RandomCompat.WeightedChoice({ 'choices': choices, 'weights': weights, 'unit': i }));
         xs[i] = a.get('x');
       }
       return xs;
@@ -194,12 +286,68 @@ describe('Test randomization ops', function() {
     testDistributions();
   });
 
+  it('works for sample (compat)', function() {
+    var N = 100;
+    function sample(choices, draws) {
+      var xs = [];
+      for (var i = 0; i < N; i++) {
+        var a = new AssignmentCompat(choices.join(', '));
+        a.set('x', new RandomCompat.Sample({ 'choices': choices, 'draws': draws, 'unit': i }));
+        xs[i] = a.get('x');
+      }
+      return xs;
+    }
+
+    function listDistributionTester(xsList, valueMass, N) {
+      var valueDensity = valueMassToDensity(valueMass);
+      var l = [];
+
+      /* bad equivalent to zip() from python */ 
+      xsList.forEach(function(xs, i){
+        xs.forEach(function(x, j) {
+          if (!l[j]) {
+            l[j] = [x];
+          } else {
+            l[j].push(x);
+          }
+        });
+        if (i === xsList.length-1) {
+          l.forEach(function(el) {
+            assertProbs(el, valueDensity, N);
+          });
+        }       
+      });
+    }
+
+    function testDistributions() {
+      var a = [1, 2, 3];
+      var ret = [{1: 1}, {2: 1}, {3: 1}];
+      listDistributionTester(sample(a, 3), ret, N);
+      listDistributionTester(sample(a, 2), ret, N);
+      a = ['a', 'a', 'b'];
+      ret = [{'a': 2}, {'b': 1}];
+      listDistributionTester(sample(a, 3), ret, N);
+    }
+
+    testDistributions();
+  });
+
   it('works with more efficient sample', function() {
     var choices = [1, 2, 3, 4, 5, 6, 7];
     var draws = 5;
 
     var a = new Assignment(choices.join(', '));
     a.set('x', new Random.Sample({ 'choices': choices, 'draws': draws, 'unit': '1' }));
+    var x = a.get('x');
+    expect(x.length).toEqual(5);
+  });
+
+  it('works with more efficient sample (compat)', function() {
+    var choices = [1, 2, 3, 4, 5, 6, 7];
+    var draws = 5;
+
+    var a = new AssignmentCompat(choices.join(', '));
+    a.set('x', new RandomCompat.Sample({ 'choices': choices, 'draws': draws, 'unit': '1' }));
     var x = a.get('x');
     expect(x.length).toEqual(5);
   });
